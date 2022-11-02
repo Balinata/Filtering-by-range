c15ce44114e3ac96a1b33bdbe7fb8b8ce3c8e0da
natali <devidvarl@gmail.com>
2022-11-02 15:06:14 +0100

    format selection

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function filterRange(arr, a, b) {
     return arr.filter(item => (a <= item && item <= b));
-  }
-  let arr = [10, 130, 70, 12];
-  let filtered = filterRange(arr, 10, 50);
-  alert( filtered );
-  alert( arr );
+}
+let arr = [10, 130, 70, 12];
+let filtered = filterRange(arr, 10, 50);
+alert(filtered);
+alert(arr);
